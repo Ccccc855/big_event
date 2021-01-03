bd7b66f229027893c3a14a7e75d5393b4157135c
gouzi <958440824@qq.com>
2021-01-03 18:57:00 +0800

    完成了主页的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -50,7 +50,7 @@ $(function () {
                 // 将登录成功得到的 token 字符串，保存到 localStorage 中
                 localStorage.setItem('token', res.token)
                 // 跳转到后台主页
-                location.href = '/login.html'
+                location.href = '/index.html'
             }
         });
     })
